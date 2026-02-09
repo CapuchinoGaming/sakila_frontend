884e7aa18895ba4ad987f6d2d28baa1a4a1a7c2b
CapuchinoGaming <127352818+CapuchinoGaming@users.noreply.github.com>
2026-02-09 05:33:03 -0500

    feat: create customer webpage layout
    Created multiple React components on App.jsx.
    Added navigation bar that sits on top of the website.
    Added customer table to display queried customers:
    - Displays up to 3 customers at a time.
    - Table scrolls down if more than 4 customers were fetched.
    Added rental history table to display a customer's rental history.
    Added customer information block to display a customer's information.
    Applied initial styling in index.css

---
+++ thing.jsx
@@ -0,0 +1,72 @@
+// Product Page
+<div style={{width: '100%', height: '100%', position: 'relative', background: 'white', overflow: 'hidden'}}>
+    {/* Page Heading */}
+    <div style={{width: 1440, height: 149, left: 0, top: 112, position: 'absolute', background: 'white', overflow: 'hidden'}}>
+        <div style={{width: 88, height: 40, left: 1077, top: 76, position: 'absolute', background: '#426B1F', overflow: 'hidden', borderRadius: 20}}>
+            <div style={{left: 16, top: 9.50, position: 'absolute', textAlign: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: 'white', fontSize: 16, fontFamily: 'Inter', fontWeight: '600', lineHeight: 20.80, wordWrap: 'break-word'}}>Default</div>
+        </div>
+        <div style={{width: 62, height: 40, left: 1173, top: 76, position: 'absolute', background: 'white', overflow: 'hidden', borderRadius: 20, outline: '1px #C2C2C2 solid', outlineOffset: '-1px'}}>
+            <div style={{left: 16, top: 9.50, position: 'absolute', textAlign: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: 'black', fontSize: 16, fontFamily: 'Inter', fontWeight: '600', lineHeight: 20.80, wordWrap: 'break-word'}}>A-Z</div>
+        </div>
+        <div style={{width: 101, height: 40, left: 1243, top: 76, position: 'absolute', background: 'white', overflow: 'hidden', borderRadius: 20, outline: '1px #C2C2C2 solid', outlineOffset: '-1px'}}>
+            <div style={{left: 16, top: 9.50, position: 'absolute', textAlign: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: 'black', fontSize: 16, fontFamily: 'Inter', fontWeight: '600', lineHeight: 20.80, wordWrap: 'break-word'}}>List view</div>
+        </div>
+        <div style={{left: 347, top: 90, position: 'absolute', justifyContent: 'flex-end', display: 'flex', flexDirection: 'column'}}><span style="color: 'black', fontSize: 20, fontFamily: 'Inter', fontWeight: '500', lineHeight: 26, wordWrap: 'break-word'">Fresh</span><span style="color: 'black', fontSize: 20, fontFamily: 'Inter', fontWeight: '300', lineHeight: 26, wordWrap: 'break-word'">  —  August 21, 2023</span></div>
+        <div style={{left: 96, top: 56, position: 'absolute', justifyContent: 'flex-end', display: 'flex', flexDirection: 'column', color: 'black', fontSize: 64, fontFamily: 'Newsreader', fontWeight: '400', lineHeight: 76.80, wordWrap: 'break-word'}}>Produce</div>
+        <div style={{width: 1248, height: 0, left: 96, top: 149, position: 'absolute', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', outline: '2px #E6E6E6 solid', outlineOffset: '-1px'}}></div>
+    </div>
+
+    {/* Navigation Bar */}
+    <div style={{width: 1440, height: 112, left: 0, top: 0, position: 'absolute', overflow: 'hidden'}}>
+        <div style={{left: 96, top: 41, position: 'absolute', color: '#426B1F', fontSize: 32, fontFamily: 'Newsreader', fontWeight: '500', lineHeight: 32, wordWrap: 'break-word'}}>
+            World Peas
+        </div>
+
+        <div style={{left: 744, top: 45.50, position: 'absolute', textAlign: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: 'black', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', lineHeight: 20.80, wordWrap: 'break-word'}}>
+            Shop
+        </div>
+
+        <div style={{left: 831, top: 45.50, position: 'absolute', textAlign: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: 'black', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', lineHeight: 20.80, wordWrap: 'break-word'}}>
+            Newstand
+        </div>
+
+        <div style={{left: 956, top: 45.50, position: 'absolute', textAlign: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: 'black', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', lineHeight: 20.80, wordWrap: 'break-word'}}>
+            Who we are
+        </div>
+
+        <div style={{left: 1094, top: 45.50, position: 'absolute', textAlign: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: 'black', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', lineHeight: 20.80, wordWrap: 'break-word'}}>
+            My profile
+        </div>
+
+        <div style={{width: 126, height: 48, left: 1218, top: 32, position: 'absolute', background: '#426B1F', overflow: 'hidden', borderRadius: 8}}>
+            <div style={{left: 23, top: 13.50, position: 'absolute', textAlign: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: 'white', fontSize: 16, fontFamily: 'Inter', fontWeight: '600', lineHeight: 20.80, wordWrap: 'break-word'}}>
+                Basket (3)
+            </div>
+        </div>
+
+    </div>
+
+    {/* Tomato */}
+    <div style={{width: 395, height: 444, left: 96, top: 301, position: 'absolute', background: '#FAFAF5', overflow: 'hidden', borderRadius: 24, outline: '2px #E6E6E6 solid', outlineOffset: '-2px'}}>
+        <div style={{left: 24, top: 320, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Inter', fontWeight: '600', lineHeight: 26, wordWrap: 'break-word'}}>Heirloom tomato</div>
+        <div style={{left: 24, top: 350, position: 'absolute', color: '#426B1F', fontSize: 20, fontFamily: 'Inter', fontWeight: '600', lineHeight: 26, wordWrap: 'break-word'}}>$5.99 / lb</div>
+        <div style={{left: 24, top: 392, position: 'absolute', color: '#6D6D6D', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', lineHeight: 24, wordWrap: 'break-word'}}>Grown in San Juan Capistrano, CA</div>
+        <img style={{width: 395, height: 296, left: 0, top: 0, position: 'absolute'}} src="https://placehold.co/395x296" />
+    </div>
+
+    {/* Ginger */}
+    <div style={{width: 395, height: 444, left: 523, top: 301, position: 'absolute', background: '#FAFAF5', overflow: 'hidden', borderRadius: 24, outline: '2px #E6E6E6 solid', outlineOffset: '-2px'}}>
+        <div style={{left: 24, top: 320, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Inter', fontWeight: '600', lineHeight: 26, wordWrap: 'break-word'}}>Organic ginger</div>
+        <div style={{left: 24, top: 350, position: 'absolute', color: '#426B1F', fontSize: 20, fontFamily: 'Inter', fontWeight: '600', lineHeight: 26, wordWrap: 'break-word'}}>$12.99 / lb</div>
+        <div style={{left: 24, top: 392, position: 'absolute', color: '#6D6D6D', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', lineHeight: 24, wordWrap: 'break-word'}}>Grown in Huntington Beach, CA</div>
+        <img style={{width: 395, height: 296, left: 0, top: 0, position: 'absolute'}} src="https://placehold.co/395x296" />
+    </div>
+
+    {/* Onion */}
+    <div style={{width: 395, height: 444, left: 950, top: 301, position: 'absolute', background: '#FAFAF5', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', overflow: 'hidden', borderRadius: 24, outline: '2px #E6E6E6 solid', outlineOffset: '-2px'}}>
+        <div style={{left: 24, top: 321, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Inter', fontWeight: '600', lineHeight: 26, wordWrap: 'break-word'}}>The Onion<br/></div>
+        <div style={{left: 24, top: 350, position: 'absolute', color: '#426B1F', fontSize: 20, fontFamily: 'Inter', fontWeight: '600', lineHeight: 26, wordWrap: 'break-word'}}>$12.99 / lb</div>
+        <div style={{left: 24, top: 392, position: 'absolute', color: '#6D6D6D', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', lineHeight: 24, wordWrap: 'break-word'}}>Trustable source of news</div>
+        <img style={{width: 395, height: 296, left: 0, top: 0, position: 'absolute'}} src="https://placehold.co/395x296" />
+    </div>
+</div>
