be8b56c988a2cae498bf277909b9789388129bbb
CapuchinoGaming <127352818+CapuchinoGaming@users.noreply.github.com>
2026-03-04 13:19:54 -0500

    style(navigation-bar): add dynamic color to navigation bar
    add dynamic color to navigation bar, color changes based on rendered webpage.
    - HomePage is green.
    - FilmsPage is blue.
    - CustomerPage is yellow.

--- my-website/src/components/NavigationBar.jsx
+++ my-website/src/components/NavigationBar.jsx
@@ -1,13 +1,24 @@
-import { Link } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 import Session from "./Session";
 
 /*  Navigation Bar
     Positioned on top of all pages.
     It contains the "logo" and has links to the other pages. */
 function NavigationBar() {
+    const { pathname } = useLocation();
+    let navStyle = { backgroundColor: "#4F7A2B" };
+
+    if (pathname == "/") {
+        navStyle = { backgroundColor: "#4F7A2B" };
+    } else if (pathname == "/films") {
+        navStyle = { backgroundColor: "#2F6FA8" };
+    } else if (pathname == "/customers") {
+        navStyle = { backgroundColor: "#D3A53A" };
+    }
+    
     return (
         <nav>
-            <div className="bar">
+            <div style={navStyle} className="bar">
                 <div className="logo">Sekila</div>
 
                 <ul className="links">
