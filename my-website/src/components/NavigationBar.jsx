696ebab0390de59e4bc7c61b28afd0d06906f4af
CapuchinoGaming <127352818+CapuchinoGaming@users.noreply.github.com>
2026-03-05 04:42:20 -0500

    style(navigation-bar): style missing components
    add dynamic color to Session component
    change styling of webpage links
    change position of components inside NavigationBar

--- my-website/src/components/NavigationBar.jsx
+++ my-website/src/components/NavigationBar.jsx
@@ -1,4 +1,4 @@
-import { useLocation, Link } from 'react-router-dom';
+import { useLocation, NavLink } from 'react-router-dom';
 import Session from "./Session";
 
 /*  Navigation Bar
@@ -6,25 +6,31 @@ import Session from "./Session";
     It contains the "logo" and has links to the other pages. */
 function NavigationBar() {
     const { pathname } = useLocation();
-    let navStyle = { backgroundColor: "#4F7A2B" };
+    let colorPalette = { backgroundColor: "#4F7A2B" };
 
     if (pathname == "/") {
-        navStyle = { backgroundColor: "#4F7A2B" };
+        colorPalette = { backgroundColor: "#4F7A2B" };
     } else if (pathname == "/films") {
-        navStyle = { backgroundColor: "#2F6FA8" };
+        colorPalette = { backgroundColor: "#2F6FA8" };
     } else if (pathname == "/customers") {
-        navStyle = { backgroundColor: "#D3A53A" };
+        colorPalette = { backgroundColor: "#D3A53A" };
     }
     
     return (
         <nav>
-            <div style={navStyle} className="bar">
+            <div className="bar" style={colorPalette}>
                 <div className="logo">Sekila</div>
 
                 <ul className="links">
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/films">Films</Link></li>
-                    <li><Link to="/customers">Customers</Link></li>
+                    <NavLink to="/" end>
+                        Home
+                    </NavLink>
+                    <NavLink to="/films">
+                        Films
+                    </NavLink>
+                    <NavLink to="/customers">
+                        Customers
+                    </NavLink>
                 </ul>
 
                 <Session/>
--- my-website/src/components/Session.jsx
+++ my-website/src/components/Session.jsx
@@ -1,10 +1,21 @@
 import { useContext } from 'react';
+import { useLocation } from 'react-router-dom';
 import { SessionContext } from '../contexts/SessionContext';
 
 function Session() {
     const {storeID, employeeID, setStoreID, setEmployeeID} = useContext(SessionContext);
+    const { pathname } = useLocation();
+    let colorPalette = { backgroundColor: "#4F7A2B" };
+
+    if (pathname == "/") {
+        colorPalette = { backgroundColor: "#4F7A2B" };
+    } else if (pathname == "/films") {
+        colorPalette = { backgroundColor: "#2F6FA8" };
+    } else if (pathname == "/customers") {
+        colorPalette = { backgroundColor: "#D3A53A" };
+    }
     return (
-        <div className="session">
+        <div className="session" style={colorPalette}>
             <p>I am</p>
             <li>Employee id={employeeID}</li>
             <p>at</p>
