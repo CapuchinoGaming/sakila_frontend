4873f7a04b30848580aac4e0cd5f8355d9a0a37e
Matthew Tujague <mjt58@njit.edu>
2026-03-05 14:41:04 -0500

    landing done

--- my-website/src/webpages/LandingPage.jsx
+++ my-website/src/webpages/LandingPage.jsx
@@ -1,7 +1,47 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import { SessionContext } from "../contexts/SessionContext";
+import { sendRequest } from "../api/handler";
+
+function T5Rentals() {
+    const { storeID } = useContext(SessionContext);
+    const [rentals, setRentals] = useState([]);
+
+    if (rentals.length === 0) {
+        sendRequest("/top_5_rentals", { store_id: storeID })
+        .then(data => setRentals(data.rentals || []))
+        .catch(console.error);
+    }
+    
+    console.log(rentals);
+
+    return (
+        <ul>
+            {rentals.map((r, i) => (
+                <li key={i}>{r.film.title} (cat={r.category}) (lang={r.language}) - {r.rental_count} rentals</li>
+            ))}
+        </ul>
+    );
+}
+
+function T5Actors() {
+    const [actors, setActors] = useState([]);
+    const { storeID } = useContext(SessionContext);
+
+    if (actors.length === 0) {
+        sendRequest("/top_5_actors", { store_id: storeID })
+            .then(data => setActors(data.actors || []))
+            .catch(console.error);
+    }
+    return (
+        <ul>
+            {actors.map((a, idx) => (
+                <li key={idx}>{a.actor_id}: {a.first_name} {a.last_name}</li>
+            ))}
+        </ul>
+    );
+}
 
 export default function LandingPage() {
-    
     
     return (
         <div>
@@ -10,11 +50,11 @@ export default function LandingPage() {
             <br /><br />
             
             <h2>Top 5 Rentals:</h2>
-            <t5Rentals />
+            <T5Rentals />
             
             <br />
             <h2>Top 5 Actors</h2>
-            <t5Actors />
+            <T5Actors />
 
         </div>
     );
